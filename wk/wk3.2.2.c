afd82291e6dec6a71af2b0b3ff30205ad56d5637
zhanghaian <yixiao_ru_lear@163.com>
2025-09-28 16:30:16 +0800

    清理不需要的二进制文件

---
+++ wk/wk3.2.2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("%d\n", 5 == 3);
+    printf("%d\n", 5 > 3);
+    printf("%d\n", 5 <= 3);
+    printf("%d\n", 7 >= 3 + 4);
+
+    return 0;
+}
---
+++ wk/wk3.2.5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int pass = 0;
+    int score = 0;
+    printf("请输入你的成绩：");
+    scanf("%d", &score);
+    if(score >= 60){
+        printf("恭喜你，及格了\n");
+    }
+    else{
+        printf("很抱歉，你没有及格\n");
+
+    }
+    printf("再见\n");
+    return 0;
+}
