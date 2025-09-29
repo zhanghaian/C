ce2c1046e95b10f79cf545a1ce4c2797359cdb3b
zhanghaian <yixiao_ru_lear@163.com>
2025-09-29 19:25:44 +0800

    add

--- wk/wk3.2.5.c
+++ wk/wk3.2.5.c
@@ -5,10 +5,10 @@ int main(){
     int score = 0;
     printf("请输入你的成绩：");
     scanf("%d", &score);
-    if(score >= 60){
+    if (score >= 60){
         printf("恭喜你，及格了\n");
     }
-    else{
+    else {
         printf("很抱歉，你没有及格\n");
 
     }
