25fcedc24fb686f93c569d0f6c9c56adffbbc8bc
zhanghaian <yixiao_ru_lear@163.com>
2025-09-30 20:15:24 +0800

    add

---
+++ class/class9.30.1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    long a = 0;
+    long b = 0;
+    long sum = 0;
+
+    scanf("%ld %ld", &a, &b);
+    sum = a + b;
+    printf("%ld\n", sum);
+
+    return 0;
+}
