e3e1b60e67d2e4f51a9d825bbb72d1b77892bf03
zhanghaian <yixiao_ru_lear@163.com>
2025-12-21 17:37:30 +0800

    12.21.0

--- SQLite0.c
+++ SQLite0.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define MAX_TABLES 10
+#define MAX_COLS 20
+#define MAX_ROWS 1000
+#define MAX_NAME 50
+#define MAX_VALUE 100
+
+typedef enum {
+    TYPE_IN,
+    TYPE_FLOAT,
+    TYPE_TEXT,
+    TYPE_DATETIME
+} DataType;
+
+typedef struct {
+    char name[MAX_NAME];
+    DataType type;
+} Column;
---
+++ SQLite_0.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_TABLES 10
+#define MAX_COLS 20
+#define MAX_ROWS 1000
+#define MAX_NAME 50
